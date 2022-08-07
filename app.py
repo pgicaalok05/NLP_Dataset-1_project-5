caf928865e2897f26fa50ccb24e0a6d507676043
Alok Raj <91530763+pgicaalok05@users.noreply.github.com>
2022-08-07 20:04:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('project_nlp-5.pkl','rb'))  
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+print(cv)
+corpus=pd.read_csv('corpus_dataset-project-5.csv')
+
+corpus1=corpus['corpus'].tolist()
+X = cv.fit_transform(corpus1).toarray()
+
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@app.route('/predict',methods=['GET'])
+def predict():
+        
+    '''
+    For rendering results on HTML GUI
+    '''
+    text = request.args.get('text')
+    text=[text]
+    input_data = cv.transform(text).toarray()
+    
+    prediction = model.predict(input_data)
+    if prediction==1:
+      result="Positive"
+    else:
+      result="Negative"
+            
+    return render_template('index.html', prediction_text='NLP Model  has predicted about the text : {}'.format(result))
+
+app.run()
+
+
+
+
